ca16bbf913c80ba474d4a44d403418648ba5b0e0
adrbessy <adrienbessy@hotmail.com>
2021-06-08 16:32:17 +0200

    add pom dependency for jar

--- src/test/java/com/tourguide_rewardCentral/unitTest/service/TestRewardCentralServiceImpl.java
+++ src/test/java/com/tourguide_rewardCentral/unitTest/service/TestRewardCentralServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import rewardCentral.RewardCentral;
 
+
 @SpringBootTest()
 public class TestRewardCentralServiceImpl {
 
